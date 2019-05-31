0a29619dd448338da667865bf54411a0d1048fd0
David Geo Holmes <david.geo.holmes@gmail.com>
2019-05-31 12:04:34 -0400

    Multiplication

--- src/E2ga.jl
+++ src/E2ga.jl
@@ -1,13 +1,13 @@
 module E2ga
 
-import Base.show, Base.+, Base.-
+import Base.show, Base.+, Base.-, Base.*
 
 struct Geometric2
     α::Float64
     x::Float64
     y::Float64
     β::Float64
-end # struct
+end
 
 function +(a::Geometric2, b::Geometric2)
     α = a.α + b.α
@@ -15,7 +15,7 @@ function +(a::Geometric2, b::Geometric2)
     y = a.y + b.y
     β = a.β + b.β
     return Geometric2(α, x, y, β)
-end # function
+end
 
 function -(a::Geometric2, b::Geometric2)
     α = a.α - b.α
@@ -23,10 +23,18 @@ function -(a::Geometric2, b::Geometric2)
     y = a.y - b.y
     β = a.β - b.β
     return Geometric2(α, x, y, β)
-end # function
+end
+
+function *(a::Geometric2, b::Geometric2)
+    α = a.α * b.α + a.x * b.x + a.y * b.y - a.β * b.β
+    x = a.α * b.x + a.x * b.α - a.y * b.β + a.β * b.y
+    y = a.α * b.y + a.x * b.β + a.y * b.α - a.β * b.x
+    β = a.α * b.β + a.x * b.y - a.y * b.x + a.β * b.α
+    return Geometric2(α, x, y, β)
+end
 
 function show(io::IO, m::Geometric2)
     print(io, "$(m.α) + $(m.x)e1 + $(m.y)e2 + $(m.β)I")
-end # function
+end
 
-end # module
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,16 @@ end
     @test c.β == 4
 end
 
+@testset "*" begin
+    a = E2ga.Geometric2(2, 5, 11, 17)
+    b = E2ga.Geometric2(1, 3, 7, 13)
+    c = a * b
+    @test c.α == (2 * 1) + (5 * 3) + (11 * 7) - (17 * 13)
+    @test c.x == (2 * 3) + (5 * 1) - (11 * 13) + (17 * 7)
+    @test c.y == (2 * 7) + (5 * 13) + (11 * 1) - (17 * 3)
+    @test c.β == (2 * 13) + (5 * 7) - (11 * 3) + (17 * 1)
+end
+
 @testset "show" begin
     a = E2ga.Geometric2(2, 5, 11, 17)
     @test "$(a)" == "2.0 + 5.0e1 + 11.0e2 + 17.0I"
